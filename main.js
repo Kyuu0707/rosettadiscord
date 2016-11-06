2faa1bccc21bdddea58e5926eabfb00fbd019e42
Frostik7 <qlosborn@yahoo.com>
2016-11-06 10:07:36 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -4,13 +4,43 @@ var bot = new Discord.Client();
 
 var onMessage = function(message)
 {
-	console.log("Recieved message.");
+	console.log("Awaiting message.");
+	//make sure servers are up
+	if(!message.guild.available){
+		console.log("Guild not available. Returning.");
+		return;
+	}
+	
 	if(message.content === "Hi")
 	{
 		//bot.reply(message,"Suh dude");
 		message.channel.sendMessage(message.author + ", suh dude");
 	}
+	
+	else if(message.content === "-dab")
+	{
+		message.channel.sendMessage("https://68.media.tumblr.com/ded624f61eed13f2ef204ea2a49cbac7/tumblr_o7v89xiUng1qkjs22o1_540.png");
+	}
+	
+	/*Do things for SoundCloud, YT here. Need to handle parsing link,
+	downloading music, playing music through voice channel (can make
+	single function for both SC and YT if it doesn't exist already).
+	Also need to handle queueing/dequeueing songs, leaving VC, etc.
+	Need to handle bad link, give user a message as to why it was bad.*/
+	else if(message.content.substring(0,3) === "-yp"){
+		message.channel.sendMessage("Sent YouTube enqueue/play message.");
+		//Make sure user is in some voice channel
+		//if(guild.channels)
+			
+		//Search youtube with given terms, or link. If link, take youtube.com or youtu.be
+		//Download mp3
+		//Join channel and play
+	}
+	else if(message.content.substring(0,3) === "-sp"){
+		message.channel.sendMessage("Sent SoundCloud enqueue/play message.");
+	}
 }
 bot.on("message",onMessage);
+console.log("Logged in to Discord.")
 
 bot.login("MjQ0Njc2Njg0MDEwMzU2NzM2.CwBueQ.KJvVU9T6UcZsrNU0EQXE32Ns8w8");
