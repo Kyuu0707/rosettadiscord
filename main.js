aff41995846f59d32cb1db03805c3d28caa06218
Frostik7 <qlosborn@yahoo.com>
2016-12-09 14:17:30 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var bot = new Discord.Client();
 
 var onMessage = function(message)
 {
-	console.log("Got message.");
+	console.log("Got message. " + message.author.id);
 	//console.log(message.guild.fetchMembers());
 	//console.log(JSON.stringify(message.guild));
 	
@@ -25,7 +25,13 @@ var onMessage = function(message)
 	
 	else if(message.content === "-dab")
 	{
-		message.channel.sendMessage("https://68.media.tumblr.com/ded624f61eed13f2ef204ea2a49cbac7/tumblr_o7v89xiUng1qkjs22o1_540.png");
+		//message.channel.sendMessage("https://68.media.tumblr.com/ded624f61eed13f2ef204ea2a49cbac7/tumblr_o7v89xiUng1qkjs22o1_540.png");
+		message.channel.sendMessage(dab());
+	}
+	
+	else if(message.content === "-boi")
+	{
+		message.channel.sendMessage(boi());
 	}
 	
 	/*Do things for SoundCloud, YT here. Need to handle parsing link,
@@ -33,18 +39,82 @@ var onMessage = function(message)
 	single function for both SC and YT if it doesn't exist already).
 	Also need to handle queueing/dequeueing songs, leaving VC, etc.
 	Need to handle bad link, give user a message as to why it was bad.*/
-	else if(message.content.substring(0,3) === "-yp")
+	
+	//THIS WILL EVENTUALLY BE YOUTUBE
+	/*else if(message.content.substring(0,3) === "-yp")
 	{
-		message.channel.sendMessage("Sample YouTube enqueue/play message.");
+		//message.channel.sendMessage("Sample YouTube enqueue/play message.");
 		playMusic(message);
 		//Search youtube with given terms, or link. If link, take youtube.com or youtu.be
 		//Download mp3
 		//Play
+	}*/
+	
+	//GBF 10 draw sim
+	else if(message.content === "-10draw")
+	{
+		if (message.author.id === "131243763325468672")
+		{
+			message.channel.sendMessage(message.author + ", you got\n" + Gbf.roll10saber());
+		}
+		//Q ID
+		/*else if(message.author.id === "155776304573186049")
+		{
+			console.log("meme");
+			message.channel.sendMessage(message.author + ", you got\n" + Gbf.roll10Legfest());
+		}*/
+		else
+		{
+			//message.channel.sendMessage(message.author + ", you got\n" + Gbf.roll10());
+			message.channel.sendMessage(message.author + ", you got\n" + Gbf.roll10Legfest());
+		}
 	}
 	
-	else if(message.content.substring(0,7) === "-10draw")
+	//GBF yolo roll sim
+	else if(message.content === "-yolo")
 	{
-		message.channel.sendMessage(Gbf.roll10());
+		//message.channel.sendMessage(message.author+ ", you got\n" + Gbf.roll());
+		message.channel.sendMessage(message.author+ ", you got\n" + Gbf.rollLegfest());
+	}
+	
+	else if(message.content === "-r")
+	{
+		message.channel.sendMessage("http://i.imgur.com/pldCPGl.jpg");
+	}
+	
+	else if(message.content === "-ssr")
+	{
+		message.channel.sendMessage("http://i.imgur.com/YN66Eit.jpg");
+	}
+	
+	
+	//ayy lmao
+	else if(message.content === "ayy")
+	{
+		message.channel.sendMessage("lmao");
+	}
+	
+	//flipchest gif
+	else if(message.content === "-flip")
+	{
+		message.channel.sendMessage("http://i.imgur.com/xccBXHz.gif");
+	}
+	
+	//roast
+	else if(message.content === "-roast")
+	{
+		message.channel.sendMessage("http://i.imgur.com/efTdwLQ.jpg");
+	}
+	
+	//rust
+	else if(message.content === "-1v1")
+	{
+		message.channel.sendMessage("http://i.imgur.com/o6D7uJf.jpg");
+	}
+	
+	else if(message.content === "-daan")
+	{
+		message.channel.sendMessage("https://cdn.discordapp.com/attachments/199575382670835712/223691222412296192/1.gif <@149096851335938049>");
 	}
 	
 	/* SOUNDCLOUD
@@ -60,7 +130,7 @@ var playMusic = function(message)
         //console.log(JSON.stringify(member));
         member.voiceChannel.join()
         .then(connection => {
-            const dispatcher = connection.playFile("./song.mp3");
+            const dispatcher = connection.playFile("./meme/noproblem.mp3");
 			console.log("Playing...");
         })
         .catch(err => {
@@ -68,6 +138,54 @@ var playMusic = function(message)
         });
     });
 }
+
+var dab = function()
+{
+	switch(Math.floor(Math.random()*8))
+	{
+		case 0:
+			return "https://68.media.tumblr.com/ded624f61eed13f2ef204ea2a49cbac7/tumblr_o7v89xiUng1qkjs22o1_540.png";
+			break;
+		case 1:
+			return "http://i.imgur.com/HzhO1S0.jpg";
+			break;
+		case 2:
+			return "http://i.imgur.com/q3UWjcw.jpg";
+			break;
+		case 3:
+			return "https://pbs.twimg.com/media/Cop9PyZVIAAGFfI.jpg";
+			break;
+		case 4:
+			return "http://i.imgur.com/Zl0TLVb.jpg";
+			break;
+		case 5:
+			return "http://i.imgur.com/8iBC7ci.jpg";
+			break;
+		case 6:
+			return "http://puu.sh/sJYz2/4b6e451478.jpg";
+			break;
+		case 7:
+			return "http://puu.sh/sJYE2/e9d78ee368.jpg";
+			break;
+	}
+}
+
+var boi = function()
+{
+	switch(Math.floor(Math.random()*1))
+	{
+		case 0:
+			return "http://i.imgur.com/HaYBGeK.jpg";
+			break;
+		/*case 1:
+			return "";
+			break;
+		case 2:
+			return "";
+			break;*/
+	}
+}
+
 bot.on("message",onMessage);
 console.log("Logged in to Discord.")
 
--- rollsim.js
+++ rollsim.js
@@ -2,18 +2,59 @@ var rChance = .82;
 var srChance = .15;
 var ssrChance = .03;
 
-var charaSSR = ["Amira", "Cagliostro", "Charlotta", "Gawain", "Jeanne d'Arc", "Lady Grey", "Lancelot", "Nezahualpilli", "Siegfried", "SSR Vira", "Yuel", "Agielba", "Albert", "Aletheia", "SSR Aliza", "Altair", "Aoidos", "Arriet", "Arulumaya", "Ayer", "Beatrix", "Carmelina", "Catherine", "Cerberus", "Chat Noir", "Clarisse", "De La Fille", "Eustace", "SSR Feena", "SSR Ferry", "Forte", "Ghandagoza", "Hallessena", "Heles", "Izmir", "Jeanne d'Arc (Dark)", "Juliet", "Korwa", "Lennah", "Lilele", "Lily", "Melleau", "Metera (Wind)", "Metera (Fire)", "Narmaya", "Nemone", "Percival", "SSR Petra", "Romeo", "SSR Rosamia", "SSR Sara", "Sarunan (Light)", "Sarunan (Dark)", "Seruel", "Silva", "Societte (Water)", "Societte (Fire)", "Sophia", "Vampy", "Vaseraga", "Veight", "SSR Yodarha", "Yngwie", "Zahlhamelina", "Zeta"];
+var charaSSR = ["Amira", "Cagliostro", "Charlotta", "Gawain", "Jeanne d'Arc", "Lady Grey", "Lancelot", "Nezahualpilli", "Siegfried", "Vira", "Yuel", "Agielba", "Albert", "Aletheia", "Aliza", "Altair", "Anthuria", "Aoidos", "Arriet", "Arulumaya", "Ayer", "Beatrix", "Carmelina", "Catherine", "Cerberus", "Chat Noir", "Clarisse", "De La Fille", "Eustace", "Feena", "Ferry", "Forte", "Ghandagoza", "Hallessena", "Heles", "Izmir", "Jeanne d'Arc (Dark)", "Juliet", "Korwa", "Lennah", "Lilele", "Lily", "Melleau", "Mellisabelle", "Metera (Wind)", "Metera (Fire)", "Narmaya", "Nemone", "Percival", "Petra", "Romeo", "Rosamia", "Sara", "Sarunan (Light)", "Sarunan (Dark)", "Seruel", "Silva", "Societte (Water)", "Societte (Fire)", "Sophia", "Vampy", "Vaseraga", "Veight", "Yodarha", "Yngwie", "Zahlhamelina", "Zeta"];
 
-var sumSSR = ["Athena", "Macula Marius", "Medusa", "Apollo", "Vortex Dragon", "Dark Angel Olivia", "Grani", "Quetzalcoatl", "Twin Elements", "Oceanus", "Baal", "Nezha", "Siren", "Odin", "Satan", "Lich", "Cybele", "Satyr", "Neptune", "Garuda", "Prometheus", "Ca Ong", "Gilgamesh", "Morrigna", "Hector", "Anubis", "Sethlans", "Bonito", "Tezcatlipoca", "Setekh"];
+var sumSSR = ["Athena", "Macula Marius", "Medusa", "Apollo", "Vortex Dragon", "Dark Angel Olivia", "Grani", "Quetzalcoatl", "Twin Elements", "Oceanus", "Baal", "Nezha", "Siren", "Odin", "Satan", "Lich", "Cybele", "Satyr", "Neptune", "Garuda", "Prometheus", "Ca Ong", "Gilgamesh", "Morrigna", "Hector", "Anubis", "Sethlans", "Bonito", "Tezcatlipoca", "Setekh", "Typhon"];
 
 var lowWeightSumSSR = ["Agni", "Varuna", "Titan", "Zephryus", "Zeus", "Hades", "Kaguya", "Lucifer", "Bahamut", "Grand Order"];
 
+var roll = function()
+{
+	var str = "```html\n";
+	var appended;
+	var number;
+	number = Math.floor(Math.random()*99);
+	if(number < 82)
+	{
+		appended = determineR();
+	}
+	else if(82 <= number && number < 97)
+	{
+		appended = determineSR();
+	}
+	else
+	{
+		appended = "<SSR " + determineSSR() + ">";
+	}
+	return str + appended + "```";
+}
+
+var rollLegfest = function()
+{
+	var str = "```html\n";
+	var appended;
+	var number;
+	number = Math.floor(Math.random()*99);
+	if(number < 79)
+	{
+		appended = determineR();
+	}
+	else if(79 <= number && number < 94)
+	{
+		appended = determineSR();
+	}
+	else
+	{
+		appended = "<SSR " + determineSSR() + ">";
+	}
+	return str + appended + "```";
+}
+
 var roll10 = function()
 {
-	var list = "```";
+	var list = "```html\n";
 	var number;
 	var i;
-	var ssr;
 	for(i = 0;i<10;i++){
 		number = Math.floor(Math.random()*99);
 		if(i==9 && number < 97)
@@ -22,7 +63,7 @@ var roll10 = function()
 		}
 		else if(i==9 && number >= 97)
 		{
-			list = list + determineSSR() + ", ";
+			list = list + "<SSR " + determineSSR() + ">" + ", ";
 		}
 		else if(number < 82)
 		{
@@ -34,7 +75,7 @@ var roll10 = function()
 		}
 		else
 		{
-			list = list + determineSSR() + ", ";
+			list = list + "<SSR " + determineSSR() + ">" + ", ";
 		}
 		//console.log(number);
 	}
@@ -81,6 +122,88 @@ var determineSR = function()
 	}
 }
 
+var roll10saber = function()
+{
+	var list = "```html\n";
+	var number;
+	var i;
+	for(i = 0;i<10;i++){
+		number = Math.floor(Math.random()*99);
+		if(i==9 && number < 94)
+		{
+			list = list + determineSR() + ", ";
+		}
+		else if(i==9 && number >= 94)
+		{
+			list = list + "<SSR " + determineSSRsaber() + ">" + ", ";
+		}
+		else if(number < 79)
+		{
+			list = list + determineR() + ", ";
+		}
+		else if(79 <= number && number < 94)
+		{
+			list = list + determineSR() + ", ";
+		}
+		else
+		{
+			list = list + "<SSR " + determineSSRsaber() + ">" + ", ";
+		}
+		//console.log(number);
+	}
+	list = list.substring(0,list.length - 2) + "```";
+	console.log(list);
+	return list;
+}
+
+var roll10Legfest = function()
+{
+	var list = "```html\n";
+	var number;
+	var i;
+	for(i = 0;i<10;i++){
+		number = Math.floor(Math.random()*99);
+		if(i==9 && number < 94)
+		{
+			list = list + determineSR() + ", ";
+		}
+		else if(i==9 && number >= 94)
+		{
+			list = list + "<SSR " + determineSSR() + ">" + ", ";
+		}
+		else if(number < 79)
+		{
+			list = list + determineR() + ", ";
+		}
+		else if(79 <= number && number < 94)
+		{
+			list = list + determineSR() + ", ";
+		}
+		else
+		{
+			list = list + "<SSR " + determineSSR() + ">" + ", ";
+		}
+		//console.log(number);
+	}
+	list = list.substring(0,list.length - 2) + "```";
+	console.log(list);
+	return list;
+}
+
+
+var determineSSRsaber = function()
+{
+	var rand;
+	rand = Math.floor(Math.random()*9);
+	if(rand < 9) {
+		return "Yngwie";
+	}
+	else
+	{
+		return "Varuna";
+	}
+}
+
 //SR char after weight adjustment: 71
 //SR nonchar: 36
 //SR summon: 20
@@ -88,21 +211,25 @@ var determineSR = function()
 var determineSSR = function()
 {
 	var rand;
-	rand = Math.floor(Math.random()*133);
+	rand = Math.floor(Math.random()*137);
 	if(rand < 5)
 	{
 		return lowWeightSumSSR[Math.floor(Math.random()*lowWeightSumSSR.length)];
 	}
-	else if(rand > 34)
+	else if(rand > 35)
 	{
 		return charaSSR[Math.floor(Math.random()*charaSSR.length)];
 	}
 	else
 	{
-		return sumSSR[Math.floor(Math.random()*charaSSR.length)]
+		return sumSSR[Math.floor(Math.random()*sumSSR.length)]
 	}
 }
 
 module.exports = {
-	"roll10": roll10
+	"roll10": roll10,
+	"roll": roll,
+	"roll10saber": roll10saber,
+	"roll10Legfest": roll10Legfest,
+	"rollLegfest": rollLegfest
 }
